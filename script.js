b12c339d44f9d109b2f76d7e40e5e0b5dfd57966
Tushar Sharma <145897973+Tushar161616@users.noreply.github.com>
2023-12-26 19:27:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const makeChange = (c) => {
   // your name here
+	let changeobj = {
+		q : 25,
+		d : 10,
+		n : 5,
+		p : 1
+	};
+	let output = {};
+	for(let i in changeobj){
+		let cuurno = changeobj[i];
+		 output[i] = parseInt(c/cuurno);
+		c = c % cuurno;
+	}
+	return output;
 };
 
 // Do not the change the code below
